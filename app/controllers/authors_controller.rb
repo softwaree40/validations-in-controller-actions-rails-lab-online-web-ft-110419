35881fb4045c1a3daeb2d6bb565b9617c3314c88
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-05 16:04:38 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,7 +10,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(author_params)
     
-    if @author.valid?
+    if @author.save
       #binding.pry
     redirect_to author_path(@author)
    else
